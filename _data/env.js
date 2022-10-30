6a9ae779e2d69fabe00bd755df1b34c98b53ddbb
Himanshu Mahanta <50390378+himanta1012@users.noreply.github.com>
2022-10-31 01:14:23 +0530

    Update env.js

--- _data/env.js
+++ _data/env.js
@@ -1,3 +1,20 @@
 module.exports = {
   isLocal: process.env.PIXEL_ART_RULERS !== 'hide'
 };
+ewPixel,
+  getNextCoordinate,
+  getAlternativePixel,turn JSON.parse(pixelJsonString);
+}
+
+describe('pixels', () => {l: jest.fn(),
+    markdown: jest.fn(),
+    danger: {
+      git: {},
+      github: {}
+    }
+  };
+  isPixelTaken,
+  run
+} = require('../../scripts/mergePixels');
+
+const readFile = promisify(fs.readFile);
